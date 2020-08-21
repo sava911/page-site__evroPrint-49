59974ec03b43ba3f1b58748fca3c89b1f0f8ba75
andreySavchenko <58919772+sava911@users.noreply.github.com>
2020-08-21 22:42:24 +0300

    new slider

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(function () {
+    $(".banner-section__slider").slick({
+        dots: true,
+        prevArrow: '<button><img class="banner-arrow banner-arrow__prev" src="images/arrow-black-left.svg" alt=""></button>',
+        nextArrow: '<button><img class="banner-arrow banner-arrow__nex" src="images/arrow-black-right.svg" alt=""></button>',
+    });
+
+
+
+});
